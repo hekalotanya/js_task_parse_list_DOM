c74c759d0e8afde9b15e00c1a54210b3692c524c
hekalotanya <hekalot@gmail.com>
2021-08-20 17:20:14 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
 // write code here
+let employeesArray =  Array.from(document.querySelector('ul').children)
+  employeesArray.sort((a, b) => Number(a.dataset.salary.replace('$', '').replace(',','')) - Number(b.dataset.salary.replace('$', '').replace(',','')));
+  employeesArray.forEach(n => n.parentNode.appendChild(n));
+
+const getArray = [];
+
+function getEmployees(list) {
+  for (const i of list) {
+    let obj = {
+      name: i.textContent, 
+      position: i.dataset.position,
+      salary: i.dataset.salary,
+      age: i.dataset.age,
+    };
+    getArray.push(obj);
+  }
+};
+
+getEmployees(employeesArray);
